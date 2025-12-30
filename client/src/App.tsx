34f69f1c1b231b5edeffe0dceca15edabccd3239
siyad <siyad@outlook.com>
2025-12-30 21:20:19 +0000

    Cors settings for website

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,26 @@
+import { useEffect, useState } from "react"
+
 function App() {
+
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    fetch('https://localhost:5001/api/activities')
+      .then(response => response.json())
+      .then(data => setActivities(data))
+  }, []);
+
   return (
-    <h3>Reactivities</h3>
+    <>
+      <h3 style={{ color: 'red' }}>Reactivities</h3>
+      <ul>
+        {
+          activities.map((activity)=>(
+            <li key={activity.id}>{activity.title}</li>
+          ))
+        }
+      </ul>
+    </>
   )
 }
 
