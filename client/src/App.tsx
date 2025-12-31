233eddc863282c9c0057c4355be7e23bab052895
siyad <siyad@outlook.com>
2025-12-31 13:14:05 +0000

    Added types and axios

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,29 @@
+import { List, ListItem, ListItemText, Typography } from "@mui/material";
+import axios from "axios";
 import { useEffect, useState } from "react"
 
 function App() {
 
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
-    fetch('https://localhost:5001/api/activities')
-      .then(response => response.json())
-      .then(data => setActivities(data))
+    axios.get<Activity[]>('https://localhost:5001/api/activities')
+      .then(response => setActivities(response.data))
   }, []);
 
   return (
     <>
-      <h3 style={{ color: 'red' }}>Reactivities</h3>
-      <ul>
+      <Typography variant='h3'>Reactivities</Typography>
+      <List>
         {
-          activities.map((activity)=>(
-            <li key={activity.id}>{activity.title}</li>
+          activities.map((activity) => (
+            <ListItem key={activity.id}>
+              <ListItemText>{activity.title}
+              </ListItemText>
+            </ListItem>
           ))
         }
-      </ul>
+      </List>
     </>
   )
 }
